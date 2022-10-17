e351a413f4ec3face004b00ff8dbf1d34b7e88ca
Elcio Yoshiaki Sato <elciosato@gmail.com>
2022-10-17 21:36:20 +0200

    Add Swagger documentation

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,14 @@ import express from "express";
 
 import { usersRoutes } from "./routes/users.routes";
 
+import swaggerUI from "swagger-ui-express";
+
+import swaggerFile from "./swagger.json";
+
 const app = express();
 
 app.use(express.json());
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
 app.use("/users", usersRoutes);
 
